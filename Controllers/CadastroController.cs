f80b215d60d0756e1c5301e998e2d59cefb65ca1
FIC <FIC@info.local>
2021-02-12 12:07:17 -0200

    InstaDev Cadastro, HTML Inserido

---
+++ Controllers/CadastroController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Instadev.Controllers
+{
+        [Route("Cadastro")]
+    public class CadastroController : Controller
+
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Interfaces/IUsuario.cs
@@ -0,0 +1,9 @@
+using Instadev.Models;
+
+namespace Instadev.Interfaces
+{
+    public interface iUsuario
+    {
+         void Cadastrar(Usuario user);
+    }
+}
